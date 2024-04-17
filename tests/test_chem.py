0fd87cdfc6b4776f10d0276712ca86bcc73bb1a9
David Meijer <david-meijer@live.nl>
2024-04-17 10:31:09 +0200

    REF: reformatting test-suite

---
+++ tests/test_chem.py
@@ -0,0 +1,8 @@
+"""Tests for the chem module."""
+import numpy as np
+import numpy.testing as npt
+import pytest
+
+def test_dummy():
+    """Dummy test."""
+    assert 1 == 1
--- tests/test_example.py
+++
@@ -1,5 +0,0 @@
-from unittest import TestCase 
-
-class TestExample(TestCase):
-    def test_example(self):
-        self.assertTrue(True)
